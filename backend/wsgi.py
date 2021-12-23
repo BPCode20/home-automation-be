85cbf0b99fed123659a5004af47b1b5c98f0191e
matthias <matthias_herret@hotmail.com>
2021-12-23 19:37:54 +0100

    Logging

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,6 +1,7 @@
 from backend import create_app
 
 app = create_app()
-
+logging.init_logging()
+app.logger.info("Start Falsk server for Home Automation backend")
 if __name__ == "__main__":
     app.run(threaded=True)
